650ff3745ffe87ee5dacf503e69c692a63933fc8
Colloke <github.p5x18@simplelogin.co>
2022-12-30 17:16:39 -0600

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href=""
+                href="https://github.com/Colloke/danmyers.net"
                 target="_blank"
                 className="fork-btn-inner"
               >
