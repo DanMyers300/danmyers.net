db5601f6c11ad59394d3a7eb1d2ce26b1837c9f3
Colloke <github.p5x18@simplelogin.co>
2022-12-29 22:48:21 -0600

    changed name

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-56 h-56 relative overflow-hidden md:h-96 md:w-96 mb-2">
           <img src='/avatar.png' layout="fill" objectFit="cover" />
         </div>
-        <h1 className='text-3xl md:text-5xl font-medium text-teal-500'>DAMASUKMA TRIHANANDI</h1>
+        <h1 className='text-3xl md:text-5xl font-medium text-teal-500'>Daniel Myers</h1>
         <h3 className='text-xl md:text-2xl'>Cloud & Software Engineer</h3>
         <p className='text-md py-1 text-gray-800 md:text-xl'>Enthusiastic and motivated self-learning about information technology. Focused on cloud computing, devsecops, and web development. Now I am looking for more experience in my field. </p>
         <div className='text-5xl py-4 flex justify-center gap-16 text-gray-600'>
