2648ca9fc5f687f7d39286ff679ebcf49ddec702
Colloke <github.p5x18@simplelogin.co>
2022-12-30 16:49:07 -0600

    a

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,14 +14,13 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+                {" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
               <h1 className="heading-name">
-                I'M
                 <strong className="main-name"> DANIEL MYERS</strong>
               </h1>
 
