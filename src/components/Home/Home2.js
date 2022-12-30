bb6fd6ee5af3fa16abc74e7a0c8eac419ac8b437
Colloke <github.p5x18@simplelogin.co>
2022-12-30 13:52:35 -0600

    test

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,7 +4,6 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -78,16 +77,6 @@ function Home2() {
               <li className="social-icons">
                 <a
                   href=""
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/danpmyers"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
