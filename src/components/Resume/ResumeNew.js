3a209e706e0aa16650e274ec6b5f4958e75077be
Colloke <106322207+Colloke@users.noreply.github.com>
2022-12-30 16:25:30 -0600

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Colloke/danmyers.net/dev/src/Assets/Daniel-Myers-Resume.pdf";
+  "https://raw.githubusercontent.com/Colloke/danmyers.net/main/src/Assets/Daniel-Myers-Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
