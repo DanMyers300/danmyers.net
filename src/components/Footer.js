c51c51f8182379cbbed01aa367790a5b9031bb09
Colloke <github.p5x18@simplelogin.co>
2022-12-31 12:39:37 -0600

    removed face

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>*-*</h3>
+          <h3>Daniel Myers</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>{year}</h3>
