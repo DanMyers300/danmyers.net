40da201ddd53cf646da65c09e5e7029935b8bcca
Dan Myers <106322207+Colloke@users.noreply.github.com>
2022-12-31 16:25:57 -0600

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Waterloo-Greenway SMS Chatbot"
-              description="Informational chatbot for Waterloo-greenway conservatory in Austin, TX.           Used AWS Lambda and Lex to make a coversational chatbot."
+              description="Informational chatbot for Waterloo-greenway conservatory in Austin, TX. Used AWS Lambda and Lex to make a coversational chatbot."
               ghLink="https://github.com/Colloke/danmyers.net/WaterlooGreenwayChatbot"
               demoLink=""
             />
