1eec2f5ce6f4367517103f5c1aa8cd93438c928d
Colloke <github.p5x18@simplelogin.co>
2022-12-31 18:35:57 -0600

    a

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="Colloke"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#00FF00"
         fontSize={16}
       />
     </Row>
