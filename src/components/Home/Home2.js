f9fb109e4f0cad6066e5054c335240f6afae0818
Colloke <github.p5x18@simplelogin.co>
2022-12-31 19:34:29 -0600

    a

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import avatar from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -62,7 +62,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={avatar} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
