9154344641c4aa782f52422950784dba744dbc2d
Colloke <github.p5x18@simplelogin.co>
2023-02-01 17:53:49 -0600

    made the nav bar the home button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link'
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <h1 className={styles.title}>Nav</h1>
-            <p className={styles.description}>
-                <Link className={styles.navLink} href="/">Home</Link>
+            <p className={styles.titleContainer}>
+                <Link className={styles.title} href="/">Dan</Link>
+            </p>
+            <ul className={styles.description}>
                 <Link className={styles.navLink} href="/about">About</Link>
                 <Link className={styles.navLink} href="/Projects">Projects</Link>
                 <Link className={styles.navLink} href="/contact">Contact</Link>
-            </p>
+            </ul>
         </header>
     )
 }
