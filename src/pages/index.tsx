169e6a31ec31ef608027b2ee5cdc09b6b1c8e829
Colloke <github.p5x18@simplelogin.co>
2023-02-01 16:41:32 -0600

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,6 @@ export default function Index() {
     <>
       <Head>
         <title>Danmyers.net</title>
-        <meta name="description" content="Hello" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
