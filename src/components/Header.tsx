d3ed30718a1be663d92d0e01423a03828b0f8c9b
Colloke <github.p5x18@simplelogin.co>
2023-02-01 15:42:05 -0600

    made nav bar look better

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import styles from '@/styles/Header.module.css'
+import Link from 'next/link'
 
 export const Header = () => {
     return (
         <header className={styles.header}>
             <h1 className={styles.title}>Nav</h1>
             <p className={styles.description}>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/blog">Blog</a>
-                <a href="/contact">Contact</a>
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/Projects">Projects</Link>
+                <Link href="/contact">Contact</Link>
             </p>
         </header>
     )
