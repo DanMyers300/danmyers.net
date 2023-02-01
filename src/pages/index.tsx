2bbc88099cdc6fb761970c97bc51e716ce37723b
Colloke <github.p5x18@simplelogin.co>
2023-02-01 17:44:24 -0600

    A

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,25 @@ import styles from '@/styles/Index.module.css'
 import { Home } from '@/components/Home'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { loadDefaultErrorComponents } from 'next/dist/server/load-components'
 
 export default function Index() {
-
-  useEffect(() => {
-    if (document) {
-      const preLoader = document.getElementsByClassName(styles.preLoader);
-      window.addEventListener("load", () => {
-        preLoader[0].classList.add(styles.preLoaderHide);
-      });
-    }
-  }, [])
-
   return (
     <>
       <Head>
         <title>Danmyers.net</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <>
+      <header>
       <Header />
+      </header>
       <main className={styles.main}>
         <Home />
       </main>
       <footer>
         <Footer />
       </footer>
-      <div className={styles.preLoader}></div>
+      </>
     </>
   )
 }
