645a0a45155e1e8b7dde69d31b9ace8d281b7fe7
Colloke <github.p5x18@simplelogin.co>
2023-02-01 12:35:04 -0600

    added git ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c9393d4f18712b3"; }
+/******/ 		__webpack_require__.h = function() { return "38254ac41e9c6e9f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
