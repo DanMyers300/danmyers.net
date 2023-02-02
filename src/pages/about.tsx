6c6689aebcea7525d0db6589ab8b7d65e0de35f7
Colloke <github.p5x18@simplelogin.co>
2023-02-02 12:03:49 -0600

    made the header and footer global

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import styles from "@/styles/Index.module.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export default function About() {
   return (
-    <div>
-        <h1>About</h1>
-        <p>This is the about page</p>
-    </div>
+    <>
+    <Head>
+      <title>Danmyers.net</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <>
+    <header>
+    <Header />
+    </header>
+    <main className={styles.main}>
+      <h1>About</h1>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+    </>
+  </>
   );
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import styles from "@/styles/Index.module.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export default function Contact() {
   return (
-    <div>
-        <h1>Contact</h1>
-        <p>This is the contact page</p>
-    </div>
+    <>
+    <Head>
+      <title>Danmyers.net</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <>
+    <header>
+    <Header />
+    </header>
+    <main className={styles.main}>
+      <h1>Contact</h1>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+    </>
+  </>
   );
 }
