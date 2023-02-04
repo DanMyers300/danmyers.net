6c4b1add168cd88934699bd02daf8c91ab7241f0
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-04 18:50:57 +0000

    a

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <div className={styles.container}>
         <Clock />
-        <h1 className={styles.title}>Hello</h1>
+        <h1 className={styles.title}>~~ Hello ~~</h1>
         <p className={styles.description}>
             Welcome to 
             <br /> 
