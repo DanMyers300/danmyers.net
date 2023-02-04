4fccb994ece225c318700d0c80686fed3e900bbb
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-04 18:31:17 +0000

    test

---
+++ src/components/Clock.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from '@/styles/Clock.module.css'
+
+export const Clock = () => {
+    return (
+        <div className={styles.clock}>
+            <div className={`${styles.hand} ${styles.hourHand}`}></div>
+            <div className={`${styles.hand} ${styles.minHand}`}></div>
+            <div className={`${styles.hand} ${styles.secondHand}`}></div>
+        </div>
+    );
+    };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import styles from '@/styles/Home.module.css'
+import { Clock } from './Clock'
 
 export const Home = () => {
     return (
         <div className={styles.container}>
+        <Clock />
         <h1 className={styles.title}>Hello</h1>
         <p className={styles.description}>
             Welcome to 
