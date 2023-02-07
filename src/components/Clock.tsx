df0b284a3ccaeb6cbfeafb891a3a56abb0253e9b
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-02-07 21:06:35 +0000

    added clock but doesn't work

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -3,10 +3,12 @@ import styles from '@/styles/Clock.module.css'
 
 export const Clock = () => {
     return (
-        <div className={styles.clock}>
+    <div className={styles.clock}>
+        <div className={styles.clockFace}>
             <div className={`${styles.hand} ${styles.hourHand}`}></div>
             <div className={`${styles.hand} ${styles.minHand}`}></div>
             <div className={`${styles.hand} ${styles.secondHand}`}></div>
         </div>
+    </div>
     );
-    };
+};
