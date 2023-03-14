bb803d991ce58321c0428b56380059763f626567
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-14 22:37:47 +0000

    added cube

--- components/CubeComponent.tsx
+++ components/CubeComponent.tsx
@@ -2,13 +2,32 @@ import styles from '@/styles/Cube.module.css'
 
 export default function CubeCompontent() {
     return (
-        <div className={styles.cube}>
-            <div className={`${styles.face} ${styles.face1}`}>Front</div>
-            <div className={`${styles.face} ${styles.face2}`}>Back</div>
-            <div className={`${styles.face} ${styles.face3}`}>Right</div>
-            <div className={`${styles.face} ${styles.face4}`}>Left</div>
-            <div className={`${styles.face} ${styles.face5}`}>Top</div>
-            <div className={`${styles.face} ${styles.face6}`}>Bottom</div>
+        <div className={styles.wrapper} id="cubewrapper">
+        <div className={styles.poswrap}>
+        <div className={styles.floatwrap}>
+        <div className={styles.spinwrap}>
+        <div className={styles.tran1}>
+        <div className={styles.tran2}>
+            <div className={styles.cube}>
+                <div className={styles.cube_face}>
+                </div>
+                <div className={styles.cube_face}>
+                </div>
+                <div className={styles.cube_face}>
+                </div>
+                <div className={styles.cube_face}>
+                </div>
+                <div className={styles.cube_face}>
+                </div>
+                <div className={styles.cube_face}>
+                </div>
+            </div>
         </div>
+        </div>
+        </div>
+        </div>
+        </div>
+  
+</div>
     )
 }
