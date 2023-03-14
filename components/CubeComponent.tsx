24756cc121db753a81d359d295a38a5374bbb8c1
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-14 21:42:35 +0000

    working on cube

--- components/CubeComponent.tsx
+++ components/CubeComponent.tsx
@@ -1,12 +1,14 @@
+import styles from '@/styles/Cube.module.css'
+
 export default function CubeCompontent() {
     return (
-        <div className="cube">
-            <div className="cube__face cube__face--front">Front</div>
-            <div className="cube__face cube__face--back">Back</div>
-            <div className="cube__face cube__face--right">Right</div>
-            <div className="cube__face cube__face--left">Left</div>
-            <div className="cube__face cube__face--top">Top</div>
-            <div className="cube__face cube__face--bottom">Bottom</div>
+        <div className={styles.cube}>
+            <div className={`${styles.face} ${styles.face1}`}>Front</div>
+            <div className={`${styles.face} ${styles.face2}`}>Back</div>
+            <div className={`${styles.face} ${styles.face3}`}>Right</div>
+            <div className={`${styles.face} ${styles.face4}`}>Left</div>
+            <div className={`${styles.face} ${styles.face5}`}>Top</div>
+            <div className={`${styles.face} ${styles.face6}`}>Bottom</div>
         </div>
     )
 }
