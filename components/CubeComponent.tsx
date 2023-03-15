026540683192c56fb2158b01cc59606cd0865145
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-15 20:38:04 +0000

    added text going away after 2 seconds

--- components/CubeComponent.tsx
+++ components/CubeComponent.tsx
@@ -6,45 +6,52 @@ export default function CubeCompontent() {
         const info = document.getElementById('toggle') as HTMLDivElement;
         info.style.visibility = 'visible';
     }
-    
+    function hideCubeClick() {
+        const info = document.getElementById('toggle') as HTMLDivElement;
+        info.style.visibility = 'hidden';
+    }
+
     if (typeof document !== 'undefined') {
     const CubeClick= document.getElementById('cubewrapper') as HTMLDivElement;
     CubeClick?.addEventListener('click', () => {
         handleCubeClick();
     })
-    }
+    setTimeout(hideCubeClick, 2000);
+}
 
     return (
-        <div className={styles.wrapper} id="cubewrapper">
+        <>
             <div className={styles.info} id="toggle">
                 <p>
-                    Hello World!
+                    ~ CUBE ~
                 </p>
             </div>
-        <div className={styles.poswrap}>
-        <div className={styles.floatwrap}>
-        <div className={styles.spinwrap}>
-        <div className={styles.tran1}>
-        <div className={styles.tran2}>
-            <div className={styles.cube}>
-                <div className={styles.cube_face}>
-                </div>
-                <div className={styles.cube_face}>
-                </div>
-                <div className={styles.cube_face}>
-                </div>
-                <div className={styles.cube_face}>
-                </div>
-                <div className={styles.cube_face}>
-                </div>
-                <div className={styles.cube_face}>
+            <div className={styles.wrapper} id="cubewrapper">
+            <div className={styles.poswrap}>
+            <div className={styles.floatwrap}>
+            <div className={styles.spinwrap}>
+            <div className={styles.tran1}>
+            <div className={styles.tran2}>
+                <div className={styles.cube}>
+                    <div className={styles.cube_face}>
+                    </div>
+                    <div className={styles.cube_face}>
+                    </div>
+                    <div className={styles.cube_face}>
+                    </div>
+                    <div className={styles.cube_face}>
+                    </div>
+                    <div className={styles.cube_face}>
+                    </div>
+                    <div className={styles.cube_face}>
+                    </div>
                 </div>
             </div>
+            </div>
+            </div>
+            </div>
+            </div>
         </div>
-        </div>
-        </div>
-        </div>
-        </div>
-</div>
+        </>
     )
 }
