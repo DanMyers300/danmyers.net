a2e125ada09f155f084f74cf852d323636d6fc1a
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-15 23:48:28 +0000

    Made the text redirect to the resume page

--- components/Cube/CubeInfo.tsx
+++ components/Cube/CubeInfo.tsx
@@ -1,10 +1,11 @@
 import styles from '@/styles/Cube.module.css'
+import Link from 'next/link';
 
 export default function CubeInfo() {
     return (
         <div className={styles.info} id="cubeText">
-            <p className={styles.infoText}>
-                ~ CUBE ~
+            <p className={styles.infoText} id="cubeTextBody">
+            <Link href="/Resume">CUBE</Link>
             </p>
         </div>
     )
