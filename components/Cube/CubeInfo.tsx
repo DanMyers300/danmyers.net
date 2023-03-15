1ec30b1c9e62945a3207ecec06fbd8d8ca44d61f
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-15 23:58:04 +0000

    a

--- components/Cube/CubeInfo.tsx
+++ components/Cube/CubeInfo.tsx
@@ -4,9 +4,14 @@ import Link from 'next/link';
 export default function CubeInfo() {
     return (
         <div className={styles.info} id="cubeText">
-            <p className={styles.infoText} id="cubeTextBody">
-            <Link href="/Resume">CUBE</Link> {/* When you go to the resume page and then hit back it breaks the cube listeners */}
-            </p>
+
+            <Link 
+            className={styles.infoText} 
+            id="cubeTextBody" 
+            href="/Resume">
+                CUBE
+            </Link> {/* When you go to the resume page and then hit back it breaks the cube listeners */}
+            
         </div>
     )
 }
