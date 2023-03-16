6360e2cb2c6dac36e016613d15dc6d89e7590f7e
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-16 18:37:02 +0000

    changed bad javascript into react and typescript

--- components/Cube/CubeComponent.tsx
+++ components/Cube/CubeComponent.tsx
@@ -1,39 +1,58 @@
 import styles from '@/styles/Cube.module.css'
-import CubeInfo from './CubeInfo'
-import CubeLogic from './CubeLogic'
+import { useRef } from 'react';
+import Link from 'next/link'
 
 export default function CubeCompontent() {   
-    CubeLogic();
+    const cubeWrapper = useRef<HTMLDivElement>(null);
+    const info = useRef<HTMLDivElement>(null);
+    let isTextOpen = false;
+    
+    const hideCube = () => {
+        if (info.current) {
+            info.current.style.visibility = 'hidden';
+            isTextOpen = false;
+        }
+    }
+
+    const handleCubeClick = () => {
+        if (info.current) {
+            info.current.style.visibility = 'visible';
+            isTextOpen = true;
+            setTimeout(hideCube, 5000);
+        }
+    }
 
     return (
         <>
-            <CubeInfo />
-            <div className={styles.wrapper} id="cubewrapper">
-            <div className={styles.poswrap}>
-            <div className={styles.floatwrap}>
-            <div className={styles.spinwrap}>
-            <div className={styles.tran1}>
-            <div className={styles.tran2}>
-                <div className={styles.cube}>
-                    <div className={styles.cube_face}>
-                    </div>
-                    <div className={styles.cube_face}>
-                    </div>
-                    <div className={styles.cube_face}>
-                    </div>
-                    <div className={styles.cube_face}>
-                    </div>
-                    <div className={styles.cube_face}>
-                    </div>
-                    <div className={styles.cube_face}>
+            <div className={styles.info} ref={info}>        
+                <Link href="/Resume">Resume</Link>
+            </div>
+            <div onClick={handleCubeClick} className={styles.wrapper} ref={cubeWrapper}>
+                <div className={styles.poswrap}>
+                    <div className={styles.floatwrap}>
+                        <div className={styles.spinwrap}>
+                            <div className={styles.tran1}>
+                                <div className={styles.tran2}>
+                                    <div className={styles.cube}>
+                                        <div className={styles.cube_face}>
+                                        </div>
+                                        <div className={styles.cube_face}>
+                                        </div>
+                                        <div className={styles.cube_face}>
+                                        </div>
+                                        <div className={styles.cube_face}>
+                                        </div>
+                                        <div className={styles.cube_face}>
+                                        </div>
+                                        <div className={styles.cube_face}>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-            </div>
-            </div>
-            </div>
-        </div>
         </>
     )
 }
