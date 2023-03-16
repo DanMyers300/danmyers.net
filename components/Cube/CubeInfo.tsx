167125d5902c24cd04226e8138cb78b6f2b2aa77
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-03-16 16:20:44 +0000

    back button still broken

--- components/Cube/CubeInfo.tsx
+++ components/Cube/CubeInfo.tsx
@@ -1,17 +1,10 @@
 import styles from '@/styles/Cube.module.css'
-import Link from 'next/link';
+import Link from 'next/link'
 
-export default function CubeInfo() {
+export default function CubeInfo() {   
     return (
-        <div className={styles.info} id="cubeText">
-
-            <Link 
-            className={styles.infoText} 
-            id="cubeTextBody" 
-            href="/Resume">
-                CUBE
-            </Link> {/* When you go to the resume page and then hit back it breaks the cube listeners */}
-            
+        <div className={styles.info} id="cubeText">        
+            <Link href="/Resume">Resume</Link>
         </div>
     )
 }
