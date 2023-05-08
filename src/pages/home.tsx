b90430bc17e044d6472f4fcad3dd5c393ab4ae54
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-05-08 06:39:29 +0000

    began working on new self-designed website

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/Home.module.css'
+
+function HomePage() {
+
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+    }
+    
+    export default HomePage
