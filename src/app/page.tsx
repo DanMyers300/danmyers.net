f206e71075764aa338b197b90b755282434037b2
DanMyers300 <contact+github@danmyers.net>
2023-07-06 08:23:57 -0500

    added about me page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-            Welcome to Dan&apos;s Website!
+          Welcome to Dan&apos;s Website!
+        </p>
+      </div>
+      <div className={styles.aboutMe}> 
+        <p>
+          I'm Daniel Myers, a machine learning engineer and developer based in Dallas, TX. I specialize in creating innovative solutions and seamless user experiences.
+        </p>
+        <p>
+Currently, I work as a Machine Learning Engineer at FW Gasket, where I develop custom natural language processing solutions. Previously, I worked as a Developer at Musework, collaborating on cloud solutions and creating educational websites.
+        </p>
+        <p>
+I hold a Bachelor's degree in Computer Science from the University of Texas at Dallas and an Associate's degree in Business Administration from Austin Community College.
+        </p>
+        <p>
+I have expertise in Python, JavaScript, web frameworks, AWS, Azure, Google Cloud, Docker, and Kubernetes. I am dedicated to delivering high-quality results and thrive in dynamic, collaborative environments.
         </p>
       </div>
     </main>
-  )
+  );
 }
