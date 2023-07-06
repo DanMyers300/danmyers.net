b8ae242bb91767048eec722afe11c0cabdb60b79
DanMyers300 <contact+github@danmyers.net>
2023-07-06 09:01:28 -0500

    added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import './globals.css'
+import Header from './header'
 
 export const metadata = {
   title: 'Dan',
   description: 'Dan',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+        <body>
+            <Header />
+            {children}
+        </body>
     </html>
   )
 }
