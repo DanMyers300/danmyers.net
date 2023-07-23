3e111ca40dc4a2112eb9eb4360beb46798e4bc7e
DanMyers300 <contact+github@danmyers.net>
2023-07-23 17:07:08 -0500

    removing resume items I don't like

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,7 +4,7 @@ import styles from './footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Footer :) 
+      Dan Myers &copy; 2023 
     </footer>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,36 +19,6 @@ export default function Home() {
         <h1 className={styles.resumeTitle}>
         EXPERIENCE
         </h1>
-        <p className={styles.experienceTitle}>
-            • FW Gasket •  ML Engineer • (remote contract) • Mar 2023 - Present
-        </p>
-        <p className={styles.experience}>
-            Developed custom natural language processing solutions to extract items from RFQ documents and import them into an Access database.
-        </p>
-        <p className={styles.experience}>
-            Utilized Python libraries like Spacy and Gensim to create custom named entity     recognition models and custom word vectors to train on proprietary data.
-        </p>
-        <p className={styles.experience}>
-            Ensured best security practices for personal development environment and any linux servers to protect valuable company data.
-        </p>
-        <p className={styles.experienceTitle}>
-            • Musework - Developer (Remote Contract)July 2022 - Present
-        </p>
-        <p className={styles.experience}>
-            Collaborated with cross-functional teams to design and implement cloud solutions using AWS, ensuring scalability, security, and high availability.
-        </p>
-        <p className={styles.experience}>
-            Utilized AWS services to develop and maintain an informational chatbot for pa    trons of the Waterloo Greenway Conservatory in Austin, TX
-        </p>
-        <p className={styles.experience}>    
-            Developed full-stack custom educational websites used by TX teachers and school children for the client: TXDOT
-        </p>
-        <p className={styles.experienceTitle}>
-           • Emler Swim School - CS Manager (Dallas, TX) Feb 2021 - Mar 2023
-        </p>
-        <p className={styles.experienceTitle}>
-            • H-E-B - Grocery Associate (Austin, TX) May 2016 - May 2020
-        </p>
         <hr/>
         <h1 className={styles.resumeTitle}>
             EDUCATION
@@ -64,13 +34,6 @@ export default function Home() {
             September 2016 - May 2018
         </p>
         <hr />
-        <h1 className={styles.resumeTitle}>
-            SKILLS
-        </h1>
-        <p className={styles.skills}>
-            • Excel/Google Sheets • Python • Javascript • Web Frameworks
-            <br/>•  Linux/Windows OS • AWS/Azure/Google Cloud • Docker/Kubernetes
-        </p>
       </div>
     </main>
   );
