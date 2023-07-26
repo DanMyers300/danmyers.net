b6aa398cd95fc8a94cd7e422a288179c966a5677
DanMyers300 <contact+github@danmyers.net>
2023-07-26 13:38:11 -0500

    added state for light/dark mode

--- js/nav.js
+++ js/nav.js
@@ -19,21 +19,38 @@ function openInNewPage(url) {
   window.open(url, '_blank');
 }
 
-
 document.addEventListener('DOMContentLoaded', function() {
   const root = document.documentElement;
   const themeToggleButton = document.querySelector('.tdnn');
 
+  // Function to toggle the theme and save the state to localStorage
   function toggleTheme() {
     root.classList.toggle('dark-theme');
-  }
-
-  themeToggleButton.addEventListener('click', () => {
     const moonElement = document.querySelector('.moon');
     moonElement.classList.toggle('sun');
+
     themeToggleButton.classList.toggle('day');
+
+    const isDarkMode = root.classList.contains('dark-theme');
+    localStorage.setItem('darkMode', isDarkMode);
+  }
+
+  // Function to load the dark mode state from localStorage
+  function loadDarkModeState() {
+    const darkModeState = localStorage.getItem('darkMode');
+    if (darkModeState === 'true') {
+      root.classList.add('dark-theme');
+      const moonElement = document.querySelector('.moon');
+      moonElement.classList.add('sun');
+      themeToggleButton.classList.add('day');
+    }
+  }
+
+  // Load the dark mode state when the page is ready
+  loadDarkModeState();
+
+  themeToggleButton.addEventListener('click', () => {
     document.body.classList.toggle('light');
     toggleTheme();
   });
 });
-
