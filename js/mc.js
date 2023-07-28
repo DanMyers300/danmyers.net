bbc135657e5e2768054c5e8eb579608305dc4345
DanMyers300 <contact+github@danmyers.net>
2023-07-28 11:51:52 -0500

    changed api url

--- js/mc.js
+++ js/mc.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const statusText = document.getElementById('statusText');
 
   let action = 'start';
-  const apiUrl = 'https://z180pb1pd3.execute-api.us-east-1.amazonaws.com/Prod';
+  const apiUrl = 'https://z180pb1pd3.execute-api.us-east-1.amazonaws.com/Prod/mc_start_stop';
 
   const updateStatusText = (text) => {
     statusText.textContent = text;
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
       const data = await response.json();
-      
+
       if (data && data.status === 'started') {
         updateStatusText('Server started');
       } else if (data && data.status === 'stopped') {
