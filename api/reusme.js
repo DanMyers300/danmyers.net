c1231ecbeb6dc32e03ea00e78c934e11eb0779c4
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2024-04-14 14:30:45 -0500

    Create reusme.js

---
+++ api/reusme.js
@@ -0,0 +1,51 @@
+export const handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    resume: {
+      "basics": {
+        "name": "Daniel Myers",
+        "label": "Software Engineer",
+        "email": "contact@danmyers.net",
+        "phone": "(469) 847-9401",
+        "url": "https://danmyers.net",
+        "summary": "Versatile full-stack developer",
+        "location": {
+          "postalCode": "75081",
+          "city": "Richardson",
+          "countryCode": "US",
+          "region": "Texas"
+        },
+      },
+      "work": [{
+        "name": "Statefarm",
+        "position": "Software Engineer",
+        "url": "https://statefarm.com",
+        "startDate": "2024-03-30",
+        "endDate": "present",
+        "summary": "TBD",
+        "highlights": [
+          "TBD"
+        ]
+      }],
+      //"skills": [{
+      //  "name": "Web Development",
+      //  "level": "Master",
+      //  "keywords": [
+      //    "HTML",
+      //    "CSS",
+      //    "JavaScript"
+      //  ]
+      //}],
+      "interests": [{
+        "name": "Outdoors",
+        "keywords": [
+          "Birds",
+          "Bird Watching",
+          "Hiking",
+          "Mountains"
+        ]
+      }]
+    }
+  };
+  return response;
+};
