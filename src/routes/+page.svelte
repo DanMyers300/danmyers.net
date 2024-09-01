215e487fd4dc4598bddc8852466b71ece923263b
Dan Myers <contact@danmyers.net>
2024-08-31 21:25:33 -0500

    added resume page and continuing to work on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<h1>Dan_Myers.net</h1>
+<script>
+  import Header from './header.svelte';
+</script>
+
+<Header />
+<body>
+  <h1>Dan_Myers.net</h1>
+</body>
+
+<style>
+  body {
+    margin: 0; 
+    height: 100vh; 
+    display: flex;
+    flex-direction: column; 
+    justify-content: center; 
+    align-items: center; 
+    text-align: center; 
+    padding: 2em; 
+    box-sizing: border-box; 
+  }
+
+  h1 {
+    margin: 0; 
+  }
+</style>
---
+++ src/routes/header.svelte
@@ -0,0 +1,39 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/resume">Resume</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    display: flex;
+    padding-top: 1em;
+    justify-content: center;
+  }
+
+  nav {
+    width: 20%;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  li {
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+  }
+
+  li:hover {
+    text-decoration: underline;
+  }
+</style>
