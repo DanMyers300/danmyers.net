794a497c3820c9e6ec8efe72bf8932019f32b5ba
dan <contact@danmyers.net>
2025-01-18 17:18:22 -0600

    added portfolio image/text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,16 @@
-import Hero from './Hero.tsx'
+import Hero from './Hero.tsx';
+import '../styles/Home.css';
 
 function Home() {
   return (
     <>
       <main>
         <Hero />
+        <h1 className="hero-text">Dan Myers</h1>
+        <hr className="separator-bar" />
       </main>
     </>
-  );    
-};
+  );
+}
 
 export default Home;
