a305e3677a547c803fe3c32921a9d73f58a900ae
dan <contact@danmyers.net>
2025-01-21 15:26:35 -0600

    added email link on contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,24 +1,19 @@
 import '../styles/Contact.css';
+import { MdEmail } from 'react-icons/md';
 
 function Contact() {
   return (
     <main>
       <h1>Contact Me</h1>
-      <form className="contact-form">
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
+      <br />
+      <div className="contact-container">
+        <a href="mailto:contact@danmyers.net" className="email-link" target="_blank" rel="noopener noreferrer">
+          contact@danmyers.net
+        </a>
+        <a href="mailto:contact@danmyers.net" className="mail-link" target="_blank" rel="noopener noreferrer">
+          <MdEmail className="mail-icon" />
+        </a>
+      </div>
     </main>
   );
 }
