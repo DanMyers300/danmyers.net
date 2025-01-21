3304e9306db49b583f97e06c413e3e6097fbbc97
dan <contact@danmyers.net>
2025-01-21 13:12:20 -0600

    added image resize useEffect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,29 @@
-import '../styles/Hero.css'
+import React, { useEffect, useState } from 'react';
+import '../styles/Hero.css';
 
 function Hero() {
+  const [isSmallHeight, setIsSmallHeight] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallHeight(window.innerHeight < 620);
+    };
+
+    handleResize(); // Check on initial render
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <img src="/Hero.png" alt="Hero Image" class="hero-image" />
+      {!isSmallHeight && (
+        <img src="/Hero.png" alt="Hero Image" className="hero-image" />
+      )}
     </>
-  );    
-};
+  );
+}
 
 export default Hero;
