fd5e99d7855e105f4c622d6cd7bd14d0a51154a9
dan <contact@danmyers.net>
2025-03-13 20:16:18 -0500

    added flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell.${system} = pkgs.mkShell {
+      name = "danmyers.net";
+      buildInputs = with pkgs; [
+        nodejs_23
+        tailwindcss
+      ];
+    };
+  };
+}
