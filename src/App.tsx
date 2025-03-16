d6361865c04533777dcabb37d11a47fd9232fa37
dan <contact@danmyers.net>
2025-03-16 12:49:30 -0500

    added hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.tsx';
 import Home from './components/Home.tsx';
 import Projects from './components/Projects.tsx';
