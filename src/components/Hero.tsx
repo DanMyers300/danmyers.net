724dbed457892b1031ccb07de98ffd801bbba349
dan <contact@danmyers.net>
2025-03-16 08:42:12 -0500

    Switched Hero to inline styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,21 @@ function Hero() {
   return (
     <>
       {!isSmallHeight && (
-        <img src={hero} alt="Hero Image" className="hero-image" />
+        <img
+          src={hero}
+          alt="Hero Image"
+          style={{
+            width: 150,
+            height: 150,
+            borderRadius: 50,
+            border: '5px solid #fff',
+            objectFit: 'cover',
+            objectPosition: 'center -50%',
+            display: 'block',
+            margin: '0 auto',
+            backgroundColor: 'white'
+          }}
+        />
       )}
     </>
   );
