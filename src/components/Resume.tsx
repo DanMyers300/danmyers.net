f24d82a55700357f4326a1b360c2777529c61b79
dan <contact@danmyers.net>
2025-05-10 12:56:42 -0500

    updated spacing

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -9,7 +9,11 @@ const Resume: React.FC = () => {
         <a
           href="/resume.pdf"
           download
-          className="mt-10 bg-gradient-to-r from-purple-600 to-violet-600 text-white px-6 py-3 rounded-lg shadow-lg hover:shadow-xl hover:from-purple-700 hover:to-violet-700 transition duration-200 inline-block"
+          className="
+          mt-10 bg-gradient-to-r from-purple-600
+          to-violet-600 text-white px-6 py-3 rounded-lg
+          shadow-lg hover:shadow-xl hover:from-purple-700
+          hover:to-violet-700 transition duration-200 inline-block"
         >
           Download Resume
         </a>
@@ -35,17 +39,17 @@ const Resume: React.FC = () => {
           </div>
           <p className="text-start italic">Software Engineer (Hybrid)</p>
           <ul className="list-disc list-inside mt-5 pl-5">
-            <li>
+            <li className="mb-5">
               Collaborated on the successful deflection of 1M+ customer contacts
               per month via AI-powered bots and self-service knowledge portals,
               exceeding team efficiency targets and reducing manual workload.
             </li>
-            <li>
+            <li className="mb-5">
               Spearheaded the reduction of technical debt by personally removing
               over 200 AWS Lambdas across environments and updating over 100
               necessary Lambdas to most recent runtime.
             </li>
-            <li>
+            <li className="mb-5">
               Designed and implemented a modular AWS Secrets Manager pipeline to
               facilitate seamless and secure transportation of secrets between AWS
               and Azure, enhancing cross-cloud integration and operational
@@ -62,31 +66,31 @@ const Resume: React.FC = () => {
           </div>
           <p className="text-start italic">Contract Full-Stack Engineer (Remote)</p>
           <ul className="list-disc list-inside mt-5 pl-5">
-            <li>
+            <li className="mb-5">
               Lead the design and development of a containerized, full-stack,
               natural language processing application.
             </li>
-            <li>
+            <li className="mb-5">
               Vectorized inventory data using ChromaDB for use in
               resource-augmented generation.
             </li>
-            <li>
+            <li className="mb-5">
               Trained custom named entity recognition language models capable of
               97% accuracy.
             </li>
-            <li>
+            <li className="mb-5">
               Created an HTML/JS front end for the chatbot utilizing a back-end
               API written in Python with Flask.
             </li>
-            <li>
+            <li className="mb-5">
               Separated each service into its own Docker container and
               orchestrated containers with Docker Compose.
             </li>
-            <li>
+            <li className="mb-5">
               Consulted management on hardware choice for natural language
               processing solutions.
             </li>
-            <li>
+            <li className="mb-5">
               Deployed containerized application on a virtualized Linux
               environment to employer hardware.
             </li>
@@ -101,23 +105,23 @@ const Resume: React.FC = () => {
           </div>
           <p className="text-start italic">Contract Full-Stack Engineer (Remote)</p>
           <ul className="list-disc list-inside mt-5 pl-5">
-            <li>
+            <li className="mb-5">
               Consulted management to plan a custom AWS chatbot for a client:
               Waterloo-Greenway Conservancy.
             </li>
-            <li>
+            <li className="mb-5">
               Lead solo development of client chatbot which utilizes Lex v2,
               lambda, SNS, Pinpoint, and Cloudwatch.
             </li>
-            <li>
+            <li className="mb-5">
               Automated testing of chatbot with local JS functions. Handled
               thousands of texts for a year without fail.
             </li>
-            <li>
+            <li className="mb-5">
               Collaborated with the design team to build ReactJS components for
               a TXDOT educational website.
             </li>
-            <li>
+            <li className="mb-5">
               Pair-programmed with a senior developer to create the final layout
               and integrate components.
             </li>
