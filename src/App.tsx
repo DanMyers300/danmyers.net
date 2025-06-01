89422c550bf64ccdc2710e73929d2b2d1b84d045
dan <contact@danmyers.net>
2025-06-01 14:36:58 -0500

    rebuilding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import Hero from './components/Hero'
+
 function App() {
   return (
     <>
+      <main className="bg-black absolute w-screen h-screen">
+
+        {/* Hero Container */}
+        <div className="flex flex-col mt-15 items-center justify-center">
+          <Hero />
+          <p className="text-white mt-5 text-2xl font-bold"> Dan Myers </p>
+          <p className="text-white mt-2"> Software Engineer </p>
+        </div>
+
+      </main>
     </>
   )
 }
