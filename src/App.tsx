47c5282390bdc42a4bf565dd2e3532c6c226699d
dan <contact@danmyers.net>
2025-06-01 14:39:23 -0500

    adding icons

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,25 @@ function App() {
           <p className="text-white mt-5 text-2xl font-bold"> Dan Myers </p>
           <p className="text-white mt-2"> Software Engineer </p>
         </div>
+        <div className="flex justify-center space-x-8 mt-8">
+        <a
+          href="https://www.linkedin.com/in/danpmyers/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 hover:text-blue-600 transition-colors"
+        >
+          <FaLinkedin className="text-4xl" />
+        </a>
+        <a
+          href="https://git.danmyers.net"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 hover:text-blue-600 transition-colors"
+        >
+          <FaGithub className="text-4xl" />
+        </a>
+      </div>
+
 
       </main>
     </>
